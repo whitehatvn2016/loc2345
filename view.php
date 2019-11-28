b62a7f5f844f44df8e72ae739f10298f6aef8827
whitehatvn2016 <57930298+whitehatvn2016@users.noreply.github.com>
2019-11-28 10:08:35 +0700

    Add files via upload

---
+++ view.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        //Refer to database 
+        $db = parse_url(getenv("DATABASE_URL"));
+        $pdo = new PDO("pgsql:" . sprintf(
+            "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+            $db["host"],
+            $db["port"],
+            $db["user"],
+            $db["pass"],
+            ltrim($db["path"], "/")
+        ));    
+        $sql = "select * from products";
+        //compile the sql
+        $stmt = $pdo->prepare($sql);
+        //execute the query on the server and return the result set
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $stmt->execute();
+        $resultSet = $stmt->fetchAll();
+    ?>
+    <ul>
+        <?php
+            foreach ($resultSet as $row) {
+                echo "<li>" .
+                    $row["name"] . '--'. $row["price"]
+                . "</li>";
+            }
+        ?>
+    </ul>
+</body>
+</html>
